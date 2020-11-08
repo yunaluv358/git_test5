2d5e8b2e9d131a5a3f7e2bdbf841afd15a9adb4e
yunaluv358 <yunaluv358@gmail.com>
2020-11-08 15:09:20 +0900

    랜덤 데이터 생성

--- src/git_test5/Main.java
+++ src/git_test5/Main.java
@@ -1,5 +1,22 @@
 package git_test5;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
 
+    static int[] createArray(int size, int minValue, int maxValue) {
+        Random random = new Random();
+        int[] a = new int[size];
+        for (int i = 0; i < a.length; ++i)
+            a[i] = random.nextInt(maxValue - minValue + 1) + 1;
+        return a;
+    }
+
+    public static void main(String[] args) {
+        int[] a = createArray(10, 1, 10);
+        Sort.sort(a);
+        System.out.println(Arrays.toString(a));
+    }
 }
+
