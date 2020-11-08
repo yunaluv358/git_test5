aebc0c8409325019c6e4d644ab00ed57c250104c
yunaluv358 <yunaluv358@gmail.com>
2020-11-08 16:07:50 +0900

    사소한 개선

--- src/git_test5/Main.java
+++ src/git_test5/Main.java
@@ -8,8 +8,9 @@ public class Main {
     static int[] createArray(int size, int minValue, int maxValue) {
         Random random = new Random();
         int[] a = new int[size];
+        int bound = maxValue - minValue + 1;
         for (int i = 0; i < a.length; ++i)
-            a[i] = random.nextInt(maxValue - minValue + 1) + 1;
+            a[i] = random.nextInt(bound) + 1;
         return a;
     }
 
@@ -19,4 +20,3 @@ public class Main {
         System.out.println(Arrays.toString(a));
     }
 }
-
